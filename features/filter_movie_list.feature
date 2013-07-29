330e6d71810faa374b0878678d49681f426bbbde
= <=>
2013-07-29 13:49:25 -0700

    almost everything working

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -22,14 +22,18 @@ Background: movies have been added to database
   And  I am on the RottenPotatoes home page
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+  Given I check the following ratings: PG, R
+  And I uncheck the following ratings: PG-13, G
+  And I press "Refresh"
+  Then I should see all movies rated: PG, R
+  And I should not see all movies rated: PG-13, G  
 
 Scenario: no ratings selected
-  # see assignment
+  When I uncheck all the ratings
+  And I press "Refresh"
+  Then I should not see any movies
 
 Scenario: all ratings selected
-  # see assignment
+  When I check all the ratings
+  And I press "Refresh"
+  Then I should see all of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,27 +2,77 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  assert page.body =~ /#{e1}.*#{e2}/m, "#{e1} isn't before #{e2}"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+Given /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split(',').each do |rating|
+    rating = "ratings_" + rating.gsub(/\s/,'')
+    if uncheck
+      uncheck(rating)
+    else
+      check(rating)
+    end
+  end
+end
+
+When /^I (un)?check all the ratings$/ do | uncheck |
+  Movie.all_ratings.each do | rating  |
+    rating = "ratings_" + rating.gsub(/\s/,'')
+    if uncheck
+      uncheck(rating)
+    else
+      check(rating)
+    end
+  end
+end
+
+Then /I should (not )?see all movies rated: (.*)/ do |neg, rating_list|
+  ratings = rating_list.split(",").each do |rating|
+      rating = rating.gsub(/\s/,'')
+    end
+  if neg
+    #ratings.each do |rating|
+       #movie = Movie.where("rating != ?", rating)
+       #movies << movie.first
+    #end
+    ratings = Movie.all_ratings - ratings
+    movies = Movie.where(:rating => ratings)
+    movies.each do |movie|
+      assert true unless page.body =~ /#{movie.title}/m
+    end
+  else
+    movies = Movie.where(:rating => ratings) 
+    movies.each do |movie|
+      assert page.body =~ /#{movie.title}/m, "#{movie.title} did not appear"
+    end
+  end
+
+end
+
+Then /I should see all of the movies/ do
+  movies = Movie.all
+  movies.each do |movie|
+    assert page.body =~ /#{movie.title}/m, "#{movie.title} did not appear"
+  end
+  #assert Movie.find(:all).length == page.body.scan(/<tr>/).length , "Not seing all movies" 
+end
+
+Then /I should not see any movies/ do
+  movies = Movie.all
+  movies.each do |movie|
+    assert true unless page.body =~ /#{movie.title}/m
+  end
 end
